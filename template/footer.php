d22c6c0734f1cb889dc150e44f20a8105b454d91
jassan <jassanmendezfuentes@gmail.com>
2022-06-22 21:07:46 -0400

    inicio proyecto

---
+++ template/footer.php
@@ -0,0 +1,18 @@
+< footer  class = " contenedor-fluido bg-oscuro fondo fijo " >
+    < div  clase = " fila " >
+        < div  class = " col-md text-light text-center py-3 " >
+           Sitio de prueba para los alumnos
+        </ div >
+    </ div >
+</ pie de página >
+    <!-- Bibliotecas de JavaScript Bootstrap
+    Los guiones, por norma general, se deben colocar antes del cierre del cuerpo.
+    Sin embargo, hay casos particulares en donde es conveniente ponerlos en el header.
+    Una práctica habitual es también poner los scripts en el header con async o defer,
+    de este modo, los scripts son obtenidos inmediatamente cuando empieza el parseo pero
+     sin bloquear el mismo.
+    -->
+    < script  src = " https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js "  integridad = " sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk "  origen cruzado = " anónimo " ></ guión >
+    < script  src = " https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js "  integridad = " sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy  " origen cruzado = " anónimo " </ guión >
+  </ cuerpo >
+</ html >
---
+++ template/header.php
@@ -0,0 +1,21 @@
+<!doctipo html>
+< html  idioma = " es " >
+  < cabeza >
+    < title >Proyecto CFT Maule</ title >
+    <!-- Metaetiquetas requeridas -->
+    < juego de caracteres meta  = " utf-8 " >
+    < meta  name = " viewport "  content = " ancho=ancho-del-dispositivo, escala-inicial=1, reducción-para-ajustar=no " >
+    <!-- Bootstrap CSS v5.2.0-beta1 -->
+    < enlace  rel = " hoja de estilo "  href = " https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css "   integridad = " sha384-0evHe/X+R7YkIZDRvuzKMRqM +OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor  " origen cruzado = " anónimo " >
+    < enlace  rel = " hoja de estilo "  href = " https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css " >
+  </ cabeza >
+  < cuerpo >
+     < div  clase = " contenedor-líquido " >
+        < div  clase = " fila " >
+            < div  clase = " col-md " >
+                < clase de encabezado  = " py-3 " >
+                    < h3  class = " text-center " >Proyecto CFT Maule</ h3 >
+                </ encabezado >
+            </ div >
+        </ div >
+     </ div >
